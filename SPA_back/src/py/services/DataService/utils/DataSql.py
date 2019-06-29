0e89c5f5eda7a1ed03f1d9b82613f16904ed336e
breezedawn <BreezeGent@gmail.com>
2019-06-30 03:14:54 +0800

    Backend Data_one return and add database api

---
+++ SPA_back/src/py/services/DataService/utils/DataSql.py
@@ -0,0 +1,33 @@
+from SPA_back.src.py import db
+from SPA_back.src.py.services.DataService import models
+from SPA_back.src.py.utils.GetLogger.logger import setup_log
+
+logger = setup_log(__name__)
+
+
+class DataSql:
+    def __init__(self):
+        self.db = db
+
+    def add_array_to_db_one(self, array):
+        try:
+            for item in array:
+                self.add_item_to_db_one(item)
+        except BaseException as e:
+            logger.exception(e)
+            return False
+
+        return True
+
+    def add_item_to_db_one(self, item):
+        # noinspection PyBroadException
+        try:
+            data_obj = models.DataOne(data=item)
+
+            self.db.session.add(data_obj)
+
+        except Exception as e:
+
+            self.db.session.rollback()
+
+            logger.warning(e)
--- SPA_back/src/py/services/DataService/views.py
+++ SPA_back/src/py/services/DataService/views.py
@@ -1,15 +1,44 @@
+import numpy as np
 from flask import jsonify
 
 from SPA_back.src.py.services.DataService import data_blue
+from SPA_back.src.py.services.DataService.utils.DataSql import DataSql
 from SPA_back.src.py.utils.GetLogger.logger import setup_log
 from SPA_back.src.py.response_code import RET, error_map
 
 logger = setup_log(__name__)
 
+data_sql = DataSql()
+
 
 @data_blue.route('/', methods=['GET'])
 def data_test():
     result = dict(code=RET.OK, message=error_map[RET.OK])
+
+    logger.info(result)
+
+    return jsonify(result)
+
+
+@data_blue.route('/data/get_array', methods=['GET'])
+def get_array():
+    data_one = None
+
+    try:
+        data_one = np.random.randint(0, 100, 10).tolist()
+
+        data_sql.add_array_to_db_one(data_one)
+
+    except BaseException as e:
+        logger.exception(e)
+
+        result = dict(code=RET.SERVERERR, message=error_map[RET.SERVERERR], data=data_one)
+
+        logger.info(result)
+
+        return jsonify(result)
+
+    result = dict(code=RET.OK, message=error_map[RET.OK], data=data_one)
 
     logger.info(result)
 
